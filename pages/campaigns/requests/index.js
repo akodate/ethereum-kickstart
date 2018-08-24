ecd508c5b28e6d5f1473b7418096307f390a1c68
akodate <alex.kodate@gmail.com>
2018-08-24 16:28:24 +0700

    add placeholder requests page and button linking to it

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RequestsIndex extends Component {
+  render () {
+    return (
+      <h3>Request List</h3>
+    );
+  }
+}
+
+export default RequestsIndex;
