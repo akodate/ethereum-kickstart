3026c3f79b25ca3ef7b4ce107fb6b667e5e9b806
akodate <alex.kodate@gmail.com>
2018-08-24 15:50:41 +0700

    add ability to contribute to campaigns with page refresh on success

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
 import { Button, Form, Input, Message } from 'semantic-ui-react';
+import web3 from '../ethereum/web3';
+import Campaign from '../ethereum/campaign';
+import { Router } from '../routes';
 
 class ContributeForm extends Component {
-  render () {
-    return(
-      <Form>
+  state = {
+    errorMessage: '',
+    loading: false,
+    value: '',
+  }
+
+  onSubmit = async event => {
+    event.preventDefault();
+
+    const campaign = Campaign(this.props.address);
+
+    this.setState({ errorMessage: '', loading: true });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, 'ether'),
+      });
+
+      Router.replaceRoute(`/campaigns/${this.props.address}`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false, value: ''});
+  }
+
+  render() {
+    return (
+      <Form error={!!this.state.errorMessage} onSubmit={this.onSubmit}>
         <Form.Field>
           <label>Amount to Contribute</label>
-          <Input 
+          <Input
             label="ether"
             labelPosition="right"
+            onChange={event => this.setState({ value: event.target.value })}
+            value={this.state.value}
           />
         </Form.Field>
-        <Button primary>
+        <Message error header={'Oops'} content={this.state.errorMessage} />
+        <Button primary loading={this.state.loading}>
           Contribute!
         </Button>
       </Form>
